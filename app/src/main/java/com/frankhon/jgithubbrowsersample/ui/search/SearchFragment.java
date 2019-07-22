eec5ac2994e9fbf8471b9c7b8648fa1c333cd71d
v-shhong <v-shhong@microsoft.com>
2019-07-22 18:36:24 +0800

    first commit

---
+++ app/src/main/java/com/frankhon/jgithubbrowsersample/ui/search/SearchFragment.java
@@ -0,0 +1,24 @@
+package com.frankhon.jgithubbrowsersample.ui.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by Frank_Hon on 7/22/2019.
+ * E-mail: v-shhong@microsoft.com
+ */
+public class SearchFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
